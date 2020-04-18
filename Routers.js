1e6d503d2b50046d150bb5ef9e5d36826ff51ec7
90adityaCode <90adityacode@gmail.com>
2020-04-18 16:08:50 +0530

    1 file modified

--- Routers.js
+++ Routers.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./Header";
+import User from '../_container/User';
+import Blog from '../_container/Blog';
+import Admin from '../_container/Admin';
 const RouteNav = () => {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route path="/user" />
-        <Route path="/blog" />
-        <Route path="admin" />
+        <Route path="/user" component={User}/>
+        <Route path="/blog" component={Blog}/>
+        <Route path="admin" component={Admin}/>
       </Switch>
     </Router>
   );
